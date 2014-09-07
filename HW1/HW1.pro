b9e7b0ba03c8ba50781ed7c2206a289a827ff56b
Jacob Melton <jmelton116@gmail.com>
2014-09-06 21:40:31 -0400

    Commit of all Homework 1 code

---
+++ HW1/HW1.pro
@@ -0,0 +1,22 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2014-09-06T22:10:01
+#
+#-------------------------------------------------
+
+QT       += core
+
+QT       -= gui
+
+TARGET = HW1
+CONFIG   += console
+CONFIG   -= app_bundle
+
+TEMPLATE = app
+
+
+SOURCES += main.cpp \
+    person.cpp
+
+HEADERS += \
+    person.h
---
+++ HW1/person.cpp
@@ -0,0 +1,53 @@
+/////////////////////////////////////////
+// Jacob Melton
+// 905698368
+// jmelton1@vt.edu
+// ECE3574
+// Homework 1
+/////////////////////////////////////////
+
+#include "person.h"
+
+// Implementation of the Person class functions
+
+// Default constructor
+Person::Person() {
+    name = QString();
+    birthday = QDate();
+}
+
+// Constructor with name only
+Person::Person(QString newName) {
+    name = newName;
+    birthday = QDate();
+}
+
+// Constructor with name and birthday
+Person::Person(QString newName, QDate newBirthday) {
+    name = newName;
+    birthday = newBirthday;
+}
+
+// Name setter function
+void Person::setName(QString newName)
+{
+    name = newName;
+}
+
+// Birthday setter function
+void Person::setBirthday(QDate newBirthday)
+{
+    birthday = newBirthday;
+}
+
+// Name getter function
+QString Person::getName()
+{
+    return name;
+}
+
+// Birthday getter function
+QDate Person::getBirthday()
+{
+    return birthday;
+}
